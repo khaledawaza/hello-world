159d8ffe6658ef7abf9a852d812d173a08c5d0bf
khaledawaza <1khaledawaza@gmail.com>
2023-03-22 13:42:22 +0100

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,88 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
-export default class Chat extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
+  const { name } = route.params;
 
-  render() {
-    console.log("props: ", this.props);
-    const { color, name } = this.props.route.params;
-    return (
-      <View
-        style={{
-          backgroundColor: color,
-          height: "100%",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ color: "#fff", fontSize: "30" }}>Hello {name}</Text>
-      </View>
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "This is a system message",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
     );
-  }
-}
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+          left: {
+            backgroundColor: "#FFF",
+          },
+        }}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+          name,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+<TouchableOpacity
+  accessible={true}
+  accessibilityLabel="More options"
+  accessibilityHint="Lets you choose to send an image or your geolocation."
+  accessibilityRole="button"
+  onPress={onPress}
+>
+  <View style={styles.button}>...</View>
+</TouchableOpacity>;
+
+export default Chat;
