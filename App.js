31e5038451e8f02e30af16108191442fe7327125
khaledawaza <1khaledawaza@gmail.com>
2023-03-26 21:57:43 +0200

    updated project

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { View } from "react-native";
+import { Button } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
@@ -21,7 +25,7 @@ const App = () => {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   const connectionStatus = useNetInfo();
-
+  const [image, setImage] = useState(null);
   useEffect(() => {
     if (connectionStatus.isConnected === false) {
       Alert.alert("Connection Lost!");
@@ -31,25 +35,30 @@ const App = () => {
     }
   }, [connectionStatus.isConnected]);
 
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null);
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="ShoppingList">
-          {(props) => <ShoppingList db={db} {...props} />}
-        </Stack.Screen>
-        <Stack.Screen name="Chat">
-          {(props) => (
-            <Chat
-              isConnected={connectionStatus.isConnected}
-              db={db}
-              {...props}
-            />
-          )}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Button title="Pick an image from the library" onPress={pickImage} />
+      <Button title="Take a photo" onPress={() => {}} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default App;
